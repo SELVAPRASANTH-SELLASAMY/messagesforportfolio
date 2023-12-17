14a1255b71a4e6c3f3a2951334c94691726447a4
SELVAPRASANTH-SELLASAMY <prasanthsamy61@gmail.com>
2023-12-17 15:50:26 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useRef, useState } from 'react';
+import './css/App.css';
+import axios from 'axios';
+import { Helmet } from 'react-helmet';
 function App() {
+  const loader = useRef();
+  const [fetchedDatas,setFetchedDatas] = useState([]);
+  useEffect(()=>{
+    loader.current.style.display='grid';
+    axios.get("https://zany-gray-cricket-cap.cyclic.app/get")
+    .then((res)=>{
+      const response = res.data;
+      setFetchedDatas(response.map((obj)=>obj))
+      loader.current.style.display='none';
+    })
+    .catch((error)=>{
+      console.log(error)
+      loader.current.style.display='none';
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Helmet>
+        <title>Messages for SP Tech</title>
+      </Helmet>
+      <header>
+        <h1>Messages</h1>
       </header>
+      <footer>
+        {
+          fetchedDatas.map((obj)=>(
+            <div key={obj._id} className="message">
+              <div id='p' className="Name"><div className="label">Name</div> : {obj.Name}</div>
+              <div id='p' className="phoneNumber"><div className="label">Phone Number</div> : {obj.phoneNumber}</div>
+              <div id='p' className="email"><div className="label">Email</div> : <a href={'https://mail.google.com/mail/u/0/#inbox?compose='+obj.email}>{obj.email}</a></div>
+              <div id='p' className="Message"><div className="label">Message</div> : {obj.Message}</div>
+              <div id='p' className="received"><div className="label">Received at</div> : {obj.createdAt}</div>
+              <div id='p' className="edited"><div className="label">Edited at</div> : {obj.updatedAt}</div>
+            </div>
+          ))
+        }
+      </footer>
+      <div ref={loader} className="loader">
+        <div className="spin"></div>
+      </div>
     </div>
   );
 }
